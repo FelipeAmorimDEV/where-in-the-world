9eeeb4ee676e1e9a5c42fa3c69f99f50caf50f93
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-13 18:05:53 -0300

    ✅ Marcação da home

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,79 @@
 const App = () => {
- return <h1>Vite + React Boilerplate</h1>
+ return (
+  <>
+    <header><a href="/">Where in the worlds?</a> <div><span>Dark Mode</span></div></header>
+    <div className="app">
+      <form>
+        <input type="text" placeholder="Search for a country…"/>
+        <select name="region">
+          <option value="africa">Africa</option>
+          <option value="america">America</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </form>
+      <div className="countries">
+        <ul>
+          <li className="country">
+            <img src="https://flagcdn.com/be.svg" alt="The flag of Belgium is composed of three equal vertical bands of black, yellow and red." />
+            <div>
+              <h2>Belgium</h2>
+              <p><span>Population: </span> <span>81,770,900</span></p>
+              <p><span>Region: </span> <span>Europe</span></p>
+              <p><span>Capital: </span> <span>Berlin</span></p>
+            </div>
+          </li>
+          <li className="country">
+            <img src="https://flagcdn.com/be.svg" alt="The flag of Belgium is composed of three equal vertical bands of black, yellow and red." />
+            <div>
+              <h2>Belgium</h2>
+              <p><span>Population: </span> <span>81,770,900</span></p>
+              <p><span>Region: </span> <span>Europe</span></p>
+              <p><span>Capital: </span> <span>Berlin</span></p>
+            </div>
+          </li>
+          <li className="country">
+            <img src="https://flagcdn.com/be.svg" alt="The flag of Belgium is composed of three equal vertical bands of black, yellow and red." />
+            <div>
+              <h2>Belgium</h2>
+              <p><span>Population: </span> <span>81,770,900</span></p>
+              <p><span>Region: </span> <span>Europe</span></p>
+              <p><span>Capital: </span> <span>Berlin</span></p>
+            </div>
+          </li>
+          <li className="country">
+            <img src="https://flagcdn.com/be.svg" alt="The flag of Belgium is composed of three equal vertical bands of black, yellow and red." />
+            <div>
+              <h2>Belgium</h2>
+              <p><span>Population: </span> <span>81,770,900</span></p>
+              <p><span>Region: </span> <span>Europe</span></p>
+              <p><span>Capital: </span> <span>Berlin</span></p>
+            </div>
+          </li>
+          <li className="country">
+            <img src="https://flagcdn.com/be.svg" alt="The flag of Belgium is composed of three equal vertical bands of black, yellow and red." />
+            <div>
+              <h2>Belgium</h2>
+              <p><span>Population: </span> <span>81,770,900</span></p>
+              <p><span>Region: </span> <span>Europe</span></p>
+              <p><span>Capital: </span> <span>Berlin</span></p>
+            </div>
+          </li>
+          <li className="country">
+            <img src="https://flagcdn.com/be.svg" alt="The flag of Belgium is composed of three equal vertical bands of black, yellow and red." />
+            <div>
+              <h2>Belgium</h2>
+              <p><span>Population: </span> <span>81,770,900</span></p>
+              <p><span>Region: </span> <span>Europe</span></p>
+              <p><span>Capital: </span> <span>Berlin</span></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </>
+ )
 }
 
 export { App }
