048fdf32f2e257c7254a7428e8bda7bc0363a4d4
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-13 17:54:52 -0300

    ✅ Tailwind instalado e configurado com cores e fontes da aplicação

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'gray-100': '#C4C4C4',
+      'gray-400': '#2B3844',
+      'gray-600': '#202C36',
+      'gray-900': '#111517'
+    },
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
