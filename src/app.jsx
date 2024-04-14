b4e9cc3a3e4d715d3dcecfc4421bb69560eb78c7
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-13 21:58:00 -0300

    🚧 Toogle troca os temas

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const [search, setSearch] = useState('')
   const [regionOption, setRegionOption] = useState('all')
   const [loading, setLoading] = useState(null)
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState('white')
 
   const filteredCountriesByRegion = countries
     .filter(({ region }) => regionOption === 'all' ? true : region.toLowerCase() === regionOption)
@@ -40,32 +40,36 @@ const App = () => {
 
   const handleChangeSearch = (e) => setSearch(e.target.value)
   const handleChangeRegion = (e) => setRegionOption(e.target.value)
-  const handleToogleTheme = () => setTheme(prev => prev === 'dark' ? 'white' : 'dark')
+  const handleToogleTheme = () => {
+    document.body.classList.toggle('dark')
+    setTheme(prev => prev === 'dark' ? 'white' : 'dark')
+  }
 
   const formatNumber = new Intl.NumberFormat('en-US')
 
   return (
     <>
-      <header className="flex justify-between items-center py-[30px] px-4 bg-gray-400 shadow">
-        <a href="/" className="text-sm font-extrabold text-white font-sans">Where in the worlds?</a>
+      <header className="flex justify-between items-center py-[30px] px-4 bg-white dark:bg-gray-400 drop-shadow-3xl">
+        <a href="/" className="text-sm font-extrabold text-gray-900 dark:text-white font-sans">Where in the worlds?</a>
         <button onClick={handleToogleTheme} className="flex items-center gap-2">
           <Moon
             size={16}
             color={theme === 'dark' ? '#fff' : '#111517'}
             weight={theme === 'dark' ? 'fill' : 'bold'}
           />
-          <span className="font-semibold text-xs text-white">Dark Mode</span>
+          <span className="font-semibold text-xs text-gray-900 dark:text-white">Dark Mode</span>
         </button>
       </header>
       <div className="app">
         <header className="mt-6 mb-8 flex flex-col gap-10 px-4">
-          <label className="relative">
-            <MagnifyingGlass size={18} color="#ffffff" weight="bold" className="absolute top-[16px] left-[30px]" />
-            <input value={search} onChange={handleChangeSearch} type="text" placeholder="Search for a country…" className="py-[14px] px-[74px] font-sans bg-gray-400 text-white outline-none rounded-md w-full" />
+          <label className="relative drop-shadow-4xl">
+            <MagnifyingGlass size={18} color={theme === 'dark' ? '#fff' : '#111517'} weight="bold" className="absolute top-[16px] left-[30px]" />
+            <input value={search} onChange={handleChangeSearch} type="text" placeholder="Search for a country…" className="py-[14px] px-[74px] font-sans 
+            bg-white text-gray-100 dark:bg-gray-400 dark:text-white outline-none rounded-md w-full" />
           </label>
           <div className="relative w-52">
-            <CaretDown size={10} color="#ffffff" weight="bold" className="absolute top-[15px] right-[19px]" />
-            <select value={regionOption} onChange={handleChangeRegion} name="region" className="py-3 px-6 font-sans rounded-md  bg-gray-400 text-white text-xs outline-none w-full">
+            <CaretDown size={10} color={theme === 'dark' ? '#fff' : '#111517'} weight="bold" className="absolute top-[15px] right-[19px]" />
+            <select value={regionOption} onChange={handleChangeRegion} name="region" className="py-3 px-6 font-sans rounded-md bg-white text-gray-900 dark:bg-gray-400 dark:text-white text-xs outline-none w-full drop-shadow-4xl">
               <option value="all" disabled hidden>Filter by Region</option>
               <option value="all">All</option>
               <option value="africa">Africa</option>
@@ -78,16 +82,16 @@ const App = () => {
         </header>
         <div className="countries">
           <ul className="grid justify-items-center gap-10 list-none">
-            {loading && <h2 className="text-white font-bold text-2xl">{loading}</h2>}
+            {loading && <h2 className="text-gray-900 dark:text-white font-bold text-2xl">{loading}</h2>}
             {!loading && filteredCountries.length === 0 && search.length > 0 &&
               <h2 className="font-sans font-bold text-red-500 ">No countries found...</h2>
             }
             {filteredCountries.map(country => (
-              <li key={country.id} className="bg-gray-400 w-[264px] rounded-md overflow-hidden z-10">
+              <li key={country.id} className="bg-white dark:bg-gray-400 w-[264px] rounded-md overflow-hidden drop-shadow-5xl">
                 <img src={country.flags.png} alt={country.flags.alt} className="h-[160px] w-[264px]" />
-                <div className="px-6 pt-6 pb-11">
-                  <h2 className="font-sans font-extrabold text-white text-lg mb-4">{country.name.common}</h2>
-                  <div className="text-white font-sans text-sm">
+                <div className="px-6 pt-6 pb-11 text-gray-900 dark:text-white">
+                  <h2 className="font-sans font-extrabold text-lg mb-4">{country.name.common}</h2>
+                  <div className="font-sans text-sm">
                     <p>
                       <span className="font-semibold">Population: </span>
                       <span className="font-light">{formatNumber.format(country.population)}</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    dropShadow: {
+      '1xl': '0 0 7px 2px rgba(0,0,0,0.2)',
+      '3xl': '0 2px 4px 0 rgba(0,0,0,0.5)',
+      '4xl': '0 2px 9px 0 rgba(0,0,0,0.5)',
+      '5xl': '0 0 7px 2px rgba(0,0,0,0.5)'
+    },
     colors: {
       'white': '#FFFFFF',
       'gray-100': '#C4C4C4',
