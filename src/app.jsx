a0561778ed1c92f606395a18f3940562fd41fe3b
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-15 12:39:29 -0300

    ✅ Página NotFound criada

--- src/app.jsx
+++ src/app.jsx
@@ -241,12 +241,20 @@ const Country = () => {
   )
 }
 
+const NotFound = () =>
+  <div className="mt-32 text-center text-gray-900 dark:text-white">
+    <h2 className="font-bold text-9xl">404</h2>
+    <h3 className="text-3xl mt-3 mb-5">Page Not Found</h3>
+    <Link to="/" className="px-4 py-2 bg-gray-400 text-white rounded-md text-lg font-bold hover:bg-opacity-0 hover:border hover:border-gray-900 hover:text-gray-900 dark:hover:text-white">Go Home</Link>
+  </div>
+
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<DefaultLayout />} >
       <Route index element={<Navigate to="/rest-countries" />} />
       <Route path="rest-countries" element={<Home />} />
       <Route path="rest-countries/:id" element={<Country />} loader={countryLoader} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 )
